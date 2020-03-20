6d091914b1bc07ac21ebca6cc4e540ba15e595be
mariano Forti <marianodforti@gmail.com>
2020-03-20 12:32:33 -0300

    Ejercicio 5 para hacer la clase

--- Guia0/Guia0_python/Ejercicio5/Ejercicio5.py
+++ Guia0/Guia0_python/Ejercicio5/Ejercicio5.py
@@ -1,2 +1,16 @@
 # Ejecicio 5 
-# resolver un sistema lineal 
+# resolver un sistema lineal
+import numpy as np
+
+A = np.array([[ 1, -3, -2],[2,-4,-3], [-3,6,8]])
+b = np.array([[6],[8],[-5]])
+
+X = np.linalg.solve(A,b)
+
+print(X)
+
+
+ERROR = np.matmul(A,X)- b
+
+print(ERROR)
+
