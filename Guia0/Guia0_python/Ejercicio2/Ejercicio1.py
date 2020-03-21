ee4d94dcc9e1ef420368465a45e5ebf48fb722c9
mariano Forti <marianodforti@gmail.com>
2020-03-21 16:43:24 -0300

    ultimas cosas en ejerrcicio 2 de la serie, haciendo el grafico de error

--- Guia0/Guia0_python/Ejercicio2/Ejercicio1.py
+++ Guia0/Guia0_python/Ejercicio2/Ejercicio1.py
@@ -3,10 +3,10 @@ import numpy as np
 from math import factorial 
 # 
 from matplotlib import pyplot as plt
-
-
-import pdb
-
+from matplotlib import rc
+#import pdb
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text',usetex=True)
 
 def miexp(x,n):
     serie = 0
@@ -29,14 +29,20 @@ while ERR[-1] >= 1e-4:
 
 print(len(ERR))
 
-plt.semilogy(ERR)
-
+fig=plt.figure(figsize=(6, 4))
+axes=fig.add_axes([0.2, 0.2, 0.7, 0.6])
+axes.semilogy(ERR,'-o')
+plt.xlabel(r' n ')
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.ylabel(r' $$  ERR(x,n) = \Bigl \vert \frac{ f(x) - S_n (x)}{f(x)} \Bigr \vert $$ ' )
+plt.title(r'$$ S_n (x) = \sum_{i=1} ^n \frac{ x^i }{i !} $$  ', y=1.0)
 
 #x = np.linspace(-1,1,1000)
 #y = miexp(x,3)
 #plt.plot(x, y,'o')
 #plt.plot(x, np.exp(x))
 #plt.show()
-plt.savefig('figura.png') 
+plt.savefig('figura.pdf',format='pdf') 
 
 
