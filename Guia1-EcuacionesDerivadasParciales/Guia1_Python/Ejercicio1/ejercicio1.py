fd2263b2b9013c180e59cea4e3b683fbf694cdf3
mariano Forti <marianodforti@gmail.com>
2020-03-27 08:54:11 -0300

    haciendo escaleo

--- Guia1-EcuacionesDerivadasParciales/Guia1_Python/Ejercicio1/ejercicio1.py
+++ Guia1-EcuacionesDerivadasParciales/Guia1_Python/Ejercicio1/ejercicio1.py
@@ -4,55 +4,68 @@ import tycc
 import valcc
 import geo
 import MakeA as lib
+import time
 
 
-vertices = (0,  geo.Nx-1,  geo.Nx*(geo.Ny-1),  geo.Nx*geo.Ny-1)
+def makensolve(mygeo,  mytycc, myvalcc):
+    vertices = (0,  mygeo.Nx-1,  mygeo.Nx*(geo.Ny-1),  mygeo.Nx*geo.Ny-1)
 #  borde de abajo
-kc = np.linspace(vertices[0],  vertices[1], geo.Nx).astype(int)
+    kc = np.linspace(vertices[0],  vertices[1], mygeo.Nx).astype(int)
 # borde de arriba
-kd = np.linspace(vertices[2],  vertices[3], geo.Nx).astype(int)
+    kd = np.linspace(vertices[2],  vertices[3], mygeo.Nx).astype(int)
 # borde de la izquierda
-ka = np.linspace(vertices[0],  vertices[2], geo.Ny).astype(int)
+    ka = np.linspace(vertices[0],  vertices[2], mygeo.Ny).astype(int)
 # borde de la derecha
-kb = np.linspace(vertices[1],  vertices[3], geo.Ny).astype(int)
-
+    kb = np.linspace(vertices[1],  vertices[3], mygeo.Ny).astype(int)
 #  Solucion
-A, B = lib.makeA(ka, kb, kc, kd, geo, tycc, valcc)
+    A, B = lib.makeA(ka, kb, kc, kd, mygeo, tycc, valcc)
+    to = time.time()
+    T = np.linalg.solve(A, B)
+    dt = time.time()-to
 
-T = np.linalg.solve(A, B)
-Tmat = np.zeros([geo.Nx, geo.Ny])
-for i in np.linspace(0, geo.Nx-1, geo.Nx).astype(int):
-    for j in np.linspace(0, geo.Ny-1, geo.Ny).astype(int):
-        k = i+j*geo.Nx
-        Tmat[j, i] = T[k]
+    return T, dt
+
+
+def main():
+    T, dt = makensolve(geo, tycc, valcc)
+    Tmat = np.zeros([geo.Nx, geo.Ny])
+
+    for i in np.linspace(0, geo.Nx-1, geo.Nx).astype(int):
+        for j in np.linspace(0, geo.Ny-1, geo.Ny).astype(int):
+            k = i+j*geo.Nx
+            Tmat[j, i] = T[k]
 
 #  solo para graficar
-x = np.linspace(0, geo.Lx, geo.Nx).astype(float)
-y = np.linspace(0, geo.Ly, geo.Ny).astype(float)
-X, Y = np.meshgrid(x, y)
+    x = np.linspace(0, geo.Lx, geo.Nx).astype(float)
+    y = np.linspace(0, geo.Ly, geo.Ny).astype(float)
+    X, Y = np.meshgrid(x, y)
 
-CS = plt.contourf(X, Y, Tmat)
-CS2 = plt.contour(CS, levels=CS.levels, colors='k', linewidth=5)
-plt.xlabel('X')
-plt.ylabel('Y')
-cbar = plt.colorbar(CS, ticks=np.linspace(min(T), max(T), 10), boundaries=[min(T), max(T)])
-cbar.ax.set_ylabel('Temperature')
+    CS = plt.contourf(X, Y, Tmat)
+    CS2 = plt.contour(CS, levels=CS.levels, colors='k', linewidth=5)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    cbar = plt.colorbar(CS, ticks=np.linspace(min(T), max(T), 10), boundaries=[min(T), max(T)])
+    cbar.ax.set_ylabel('Temperature')
 
-plt.show()
-plt.savefig('Temperaturas-'+tycc.abajo+'.pdf')
-#plt.show()
-#plt.savefig('Temperaturas.pdf','pdf')
+    plt.show()
+    plt.savefig('Temperaturas-'+tycc.abajo+'.pdf')
+# plt.show()
+# plt.savefig('Temperaturas.pdf','pdf')
 
-np.savetxt('Temps-'+tycc.abajo+'.dat', T)
-np.savetxt('Temp_mat-'+tycc.abajo+'.dat', Tmat)
+    np.savetxt('Temps-'+tycc.abajo+'.dat', T)
+    np.savetxt('Temp_mat-'+tycc.abajo+'.dat', Tmat)
 
 
-#  Ahora tengo que calcular los flujos. 
+#  Ahora tengo que calcular los flujos.
 #  dTx,  dTy =  gradient(Tmat)
-dTy,  dTx = lib.migrad(Tmat)
-print(dTx, dTy)
+    dTy,  dTx = lib.migrad(Tmat)
+# print(dTx, dTy)
 #  VX, VY  =  meshgrid(which_plot_x, which_plot_y)
-plt.streamplot(X, Y, -dTx, -dTy, color='k', linewidth=3, density=1)
+    plt.streamplot(X, Y, -dTx, -dTy, color='k', linewidth=3, density=1)
 
-plt.show()
-plt.savefig('Temperaturas-'+tycc.abajo+'-Flujos.pdf')
+    plt.show()
+    plt.savefig('Temperaturas-'+tycc.abajo+'-Flujos.pdf')
+
+
+if __name__ == "__main__":
+    main()
---
+++ Guia1-EcuacionesDerivadasParciales/Guia1_Python/Ejercicio1/escaleo.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from ejercicio1 import makensolve
+
+Nx = 3**np.linspace(2, 4, 10)
+Nx = Nx.astype(int)
+
+
+
+print(Nx)
