0813ce644461828bdb7b33134b5bc978198c4200
mariano Forti <marianodforti@gmail.com>
2020-04-22 18:09:54 -0300

    ejercicio 3 resuelto

---
+++ Guia2-MEF01/Guia2-Python/Ejercicio3/amano.md
@@ -0,0 +1,7 @@
+# A mano con un elemento:
+C = -20 kN / cm = 20 e5 N /m
+FT = L * (-C * L ) / 2  = - C * L^2 / 2 
+FT = - 20 e5 1.5 ^2 / 2 = 22.5
+F1 = 1/3 22.5 e5  = 7.5 e5 N / m
+F2 = 2 / 3 22.5 e5 = 15 e 5 N / m
+
--- Guia2-MEF01/Guia2-Python/Ejercicio3/ejercicio3.py
+++ Guia2-MEF01/Guia2-Python/Ejercicio3/ejercicio3.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import sys
+sys.path.append("../")
 import pdb
 from scipy.integrate import quad
-sys.path.append("../")
 import mefmods as mef
 import matplotlib.pyplot as plt
-import matplotlib
+import os.path
+#import matplotlib
 #matplotlib.use('Qt5Agg')
 
 
@@ -18,48 +19,141 @@ Tengo que definir primero la generación de las fuerzas!
 
 class barra:
 
-    def ND(self, x, L):
-        return 1. - x/L
+    def __init__(self, NNODOS=2, C=20e5, L=1.5, A=10e-4, E=210E9):
+        # Inicio variables internas
+        self.NNODOS = NNODOS
+        self.U = np.zeros((NNODOS, 1), dtype=float)
+        self.F = np.zeros((NNODOS, 1), dtype=float) 
+        self.case = '{:02d}N'.format(NNODOS)
+        self.C = C
+        self.L = L
+        self.A = A
+        self.E = E
+        self.GL = 1  # 1 grado de libertad por nodo
+        # Soluciones Teóricas
+        self.X = np.linspace(0, L)
+        self.do = self.teodes(self.X)
+        self.to = self.teosig(self.X)
+        # Matrices Conectividad , nodos y Propiedades
+        self.MN = np.hstack(
+                (np.linspace(0, L, self.NNODOS, dtype=float).reshape(self.NNODOS, 1),
+                    np.zeros((self.NNODOS, 2)))
+                )
+        self.MC = np.hstack(
+                (np.linspace(0, self.NNODOS-2, self.NNODOS-1, dtype=int, axis=0),
+                    np.linspace(1, self.NNODOS-1, self.NNODOS-1, dtype=int, axis=0))
+                ).reshape((NNODOS-1, 2))
+        self.MP = self.E*self.A / (self.L / (self.NNODOS - 1)) * \
+            np.ones((NNODOS-1, 1))
+        # Matriz de rigidez
+        self.K = mef.ensamble(self.MC, self.MN, self.MP, self.GL, 1, self.case)
+        # condiciones de contorno y soluion
+        self.Fext = self.fuerza()
+        self.R = np.linspace(0, self.NNODOS-2, NNODOS-1, dtype=int)
+        self.S = np.array([NNODOS-1], dtype=int)
+        # finalmente resuelvo el problema
+        self.U, self.F = mef.resolvermef(self.R, self.S, self.K,
+                self.U[self.S], self.Fext[self.R], self.case)
+        self.Sig = self.tension()
 
-    def NI(self, x, L):
-        return x/L
+    #  Defino Funciones propias
+    def NI(self, x, l):
+        return 1. - x/l
 
-    def Tx(self, x, C):
-        return -C*x
+    def ND(self, x, l):
+        return x/l
 
-    def dfI(self, x, a, L, C):
-        return self.NI(x-a, L) * self.Tx(x, C)
+    def Tx(self, x):
+        return -self.C*x
 
-    def dfD(self, x, a, L, C):
-        return self.ND(x-a, L)*self.Tx(x, C)
+    def dfI(self, x, a, l):
+        return self.NI(x-a, l) * self.Tx(x)
 
-    def fuerza(self, MN, C):
+    def dfD(self, x, a, l):
+        return self.ND(x-a, l)*self.Tx(x)
+
+    def teodes(self, x):
+        return (x**3 - self.L**3)*abs(self.C)/(6*self.A*self.E)
+
+    def teosig(self, x):
+        return x**2 * abs(self.C) / (2*self.A)
+
+    def tension(self):
+        """ 
+        las tensiones las voy a calcular como los estiramientos por los Modulos Elasticos.
+        Debe notarse que las tensiones calculadas son Constantes dentro de cada 
+        elemento.
+        """
+        return np.diff(self.U, axis=0)*self.E/(self.L/(self.NNODOS-1))
+
+
+    def plot_teos(self, force=False):
+        """
+        test si el gráfico está hecho, luego plot del desplazamiento
+        y la tensión teóricos en pdf.
+        """
+        despfile = 'DesplazamientoTeorico.pdf'
+        sigmfile = 'TensionTeorica.pdf'
+        flag1 = os.path.exists(despfile) and os.path.isfile(despfile)
+        flag2 = os.path.exists(sigmfile) and os.path.isfile(sigmfile)
+        if (not flag1) or (not flag2) or (force):
+            x = np.linspace(0, B.L, 100)
+
+        if (not flag1) or force:
+            plt.plot(self.X, self.do, 'k')
+            plt.title('Desplazamiento Teórico')
+            plt.xlabel(r'$x (m)$')
+            plt.ylabel(r'$d(x)$ (m)')
+            plt.savefig(despfile)
+            plt.close()
+
+        if (not flag2) or force:
+            plt.plot(self.X, self.to, 'k')
+            plt.title('Tensión Teórica')
+            plt.ylabel(r'$\sigma (x)$ (Pa)')
+            plt.xlabel(r'$x$ (m)')
+            plt.savefig(sigmfile)
+            plt.close()
+
+        return
+
+    def fuerza(self):
         """
         define las fuerzas equivalentes sobre los nodos para la
         distribucuón de fuerzas T(x) = - C*x
         """
-        F = np.zeros((len(MN), 1))
-        for i in range(len(MN)-1):
+        F = np.zeros((len(self.MN), 1))
+        for i in range(len(self.MN)-1):
             a = self.MN[i, 0]
             b = self.MN[i+1, 0]
             li = b - a
-            pdb.set_trace()
-            F[i] += quad(lambda x: self.dfI(x, a, li, C), a, b)[0]
-            F[i+1] += quad(lambda x: self.dfD(x, a, li, C), a, b)[0]
+            F[i] += quad(lambda x: self.dfI(x, a, li), a, b)[0]
+            F[i+1] += quad(lambda x: self.dfD(x, a, li), a, b)[0]
         return F
 
-    def __init__(self, NNODOS=2, C=10e3, L=0.6):
-        self.MN = np.hstack(
-                (np.linspace(0, L, NNODOS, dtype=float).reshape(NNODOS, 1),
-                    np.zeros((NNODOS, 2)))
-                )
-        self.MC = np.hstack(
-                (np.linspace(0, NNODOS-1, NNODOS-1, dtype=int, axis=0),
-                    np.linspace(1, NNODOS, NNODOS-1, dtype=int, axis=0))
-                )
-        self.F = self.fuerza(self.MN, C)
+    def plot_results(self):
+        dresfile = 'ResultadoDesplazamientos-'+self.case+'.pdf'
+        plt.plot(self.MN[:, 0], self.U, '-o', label='Solucion MEF')
+        plt.plot(self.X, self.do, '--k', label='Solución Teórica')
+        plt.title('Solución Numérica para {:d} nodos'.format(self.NNODOS))
+        plt.xlabel(r'$x$ (m)')
+        plt.ylabel(r'$d(x)$ (m)')
+        plt.savefig(dresfile)
+        plt.close()
+        tresfile = 'ResultadoTensiones-'+self.case+'.pdf'
+        plt.plot(self.X, self.to, '--k', label='Solución Teórica')
+        plt.title('Solución Numérica para {:d} nodos'.format(self.NNODOS))
+        plt.xlabel(r'$x$ (m)')
+        plt.ylabel(r'$\sigma$ (GPa)')
+        TENY = np.vstack( ([0.0], self.Sig) )
+        plt.step(self.MN[:, 0], TENY, 'r',
+                where='pre', label='Solución Numérica')
+        plt.savefig(tresfile)
+        plt.close()
 
-B = barra(NNODOS=3)
-#print(B.MC)
-#print(B.MN)
-print(B.F)
+
+NODOS = [2, 3, 4, 5]
+for i in range(len(NODOS)):
+    B = barra(NODOS[i])
+    B.plot_results()
+    pdb.set_trace()
