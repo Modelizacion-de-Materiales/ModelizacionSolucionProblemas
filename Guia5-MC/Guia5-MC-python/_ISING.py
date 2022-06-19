52427c9965f95400a0994ba14b6bbb0f31dcdc20
Mariano Forti <marianodforti@gmail.com>
2022-06-19 08:40:25 +0200

    _ising wont work

--- Guia5-MC/Guia5-MC-python/_ISING.py
+++ Guia5-MC/Guia5-MC-python/_ISING.py
@@ -14,7 +14,7 @@ def ising ( NSTEPS,N,Hext,kT,J ):
 
     TRANSIENT=np.ceil(NSTEPS/10).astype(int)
        # idea sacada del paper que me paso ruben.
-    NORM = 1/(NSTEPS*N**2) 
+    NORM =NSTEPS*N**2
 
     thissize = '{:d}x{:d}'.format(N,N)
     thismcs=f'{NSTEPS}mcs'
@@ -35,28 +35,27 @@ def ising ( NSTEPS,N,Hext,kT,J ):
     # primero obtengo la suma de spines en los vecinos. 
     # el enfriamiento.
     siflips=0; noflips=0
-    for i in range(TRANSIENT):     # {
-        E, dM, s , siflips, noflips = metropolising(J,beta[1],s,siflips,noflips )
-    sumOfNeighbours = getneigbours( s )
-    E = - ( J / 2 ) * sum ( sum( s*sumOfNeighbours )) + mu*Hext*sum(s); 
-    M = sum(sum(s))
     basename='N_{:02d}'.format(N)
 
-    for t in range(len(kT)):
+    for t, b in zip(kT, beta):
          #  inicio los acumuladores
          # no hay instancia de termalización
         siflips=0;  noflips=0
-        cases= basename+'_kT_{:02d}'.format( t )
+        for i in range(TRANSIENT):     # {
+            E, dM, s , siflips, noflips = metropolising(J,b,s,siflips,noflips )
+        sumOfNeighbours = getneigbours( s )
+        E = - ( J / 2 ) * sum ( sum( s*sumOfNeighbours )) + mu*Hext*sum(s); 
+        M = sum(sum(s))
         sumOfNeighbours = getneigbours( s )
         E = - ( J / 2 ) * sum ( sum( s*sumOfNeighbours )) + mu*Hext*sum(sum(s)); 
         M = sum(sum(s))
-        siflips=0; noflips=0
          # luego del transitorio inicia los acumuladores parael caso.
         E_ACUM = E;  M_ACUM = M; E2_ACUM = E**2 ;  Mabs_ACUM = M; M2_ACUM = M**2;
+        siflips=0;  noflips=0
         for i in range(NSTEPS): # i = 1:NSTEPS         # {  %%% loop temperatura
-            dE,dM,s,siflips,noflips =  metropolising (J,beta[t],s,siflips,noflips)
+            dE,dM,s,siflips,noflips =  metropolising (J,b,s,siflips,noflips)
             sumOfNeighbours = getneigbours( s )
-#            tE = - ( J / 2 ) * sum ( sum( s*sumOfNeighbours )) + mu*Hext*sum(sum(s)); 
+            #tE = - ( J / 2 ) * sum ( sum( s*sumOfNeighbours )) + mu*Hext*sum(sum(s)); 
             E += dE  
             M += dM
             E_ACUM += E
@@ -64,18 +63,18 @@ def ising ( NSTEPS,N,Hext,kT,J ):
             M_ACUM += M
             E2_ACUM += E**2
             M2_ACUM += M**2
-        EMEAN.append( E_ACUM/NSTEPS)
-        MMEAN.append( M_ACUM/NSTEPS)
-        MabsMEAN.append(Mabs_ACUM/NSTEPS)
-        M2MEAN.append(M2_ACUM/NSTEPS)
-        E2MEAN.append(E2_ACUM/NSTEPS)
-        VARIANCE.append((E2MEAN[t]-EMEAN[t]**2)/NSTEPS  )
+        EMEAN.append( E_ACUM/NORM)
+        MMEAN.append( M_ACUM/NORM)
+        MabsMEAN.append(Mabs_ACUM/NORM)
+        M2MEAN.append(M2_ACUM/NORM**2)
+        E2MEAN.append(E2_ACUM/NORM**2)
+        VARIANCE.append((E2MEAN[-1]-EMEAN[-1]**2)/t**2/NORM  )
         SPINS.append(s)
         print(f'N = {N}, totalflips = {NSTEPS}, KT = {t}, flips = {siflips}, rejects = {noflips}')
-    data = {'T': kT,
+    data = {'T': t,
             'EMEAN': EMEAN,
             'E2MEAN': E2MEAN,
-            'CV': VARIANCE/kT**2,
+            'CV': VARIANCE,
             'MMEAN': MMEAN, 
             '|M|mean' :  MabsMEAN,
             'M2MEAN' : M2MEAN,
@@ -100,29 +99,23 @@ def getneigbours(s):
    #METROPOLIS FUNCTION                
    #  [dE,dM,s,siflips,noflips] =  ...
    #     metropolising (J,beta[t],s,siflips,noflips)
-def metropolising ( J,beta , olds, siflipin,noflipin):
+def metropolising ( J,_beta , olds, siflipin,noflipin):
     Nx,Ny = olds.shape # size(olds)
     i = np.random.randint(Nx) #randi(N)
     j = np.random.randint(Ny)
-    sdot = -copy.copy( olds [ i, j ] )
-    iup=i-Nx
-    idn=i-1
-    jup=j-Ny
-    jdn=j-1
-    # cambio de energía propuesta. 
-    dEdot = -2*(sdot)*(J)*(olds[ iup,j ] + olds[ idn,j ] + olds[ i,jup ] + olds[ i,jdn ]  )
+    dEdot = -2*(olds[i,j])*(J)*(olds[i-Nx,j ] + olds[ i-1,j ] + olds[ i,j-Ny ] + olds[ i,j-1 ]  )
     flip = False
     siflipout = siflipin
     noflipout = noflipin
-    if dEdot < 0:    
+    if dEdot < 0:
         flip=True
-    elif np.random.rand() < np.exp(-beta*dEdot):   
+    elif   np.random.rand() < np.exp(-_beta*dEdot):
         flip = True
     news=copy.copy(olds)
-    if flip == True:     # { efectivamente hago el cambio.
-        news [ i, j ] = copy.copy(sdot) 
+    if flip:     # { efectivamente hago el cambio.
+        news [ i, j ] = copy.copy(olds[i,j]) 
         dE = copy.copy(dEdot)
-        dM = 2*sdot
+        dM = -2*olds[i,j]
         siflipout=siflipout+1
     else:    
         dE = 0
